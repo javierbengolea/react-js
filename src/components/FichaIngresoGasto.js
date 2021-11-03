e18490b4a22fdac22b75e8dc639f666e4dd79439
Javier Bengolea <javierbengolea@yahoo.com.ar>
2021-11-02 23:57:12 -0300

    Mejorando ficha ingreso de datos

--- src/components/FichaIngresoGasto.js
+++ src/components/FichaIngresoGasto.js
@@ -1,8 +1,22 @@
 import "./estilos.css"
+import {nanoid} from 'nanoid'
 
 
 function Ficha() {
-    const yourDate = new Date()
+    const yourDate = new Date(new Date() - new Date(10800000))
+
+    var gasto = {
+        id: nanoid(),
+        fecha: "02/11/2021",
+        hora: "",
+        monto: "",
+        descripcion: "",
+        tipo_pago: "",
+        toString: (fecha) =>{
+            return fecha
+        }
+    };
+
 
     return (
         <article className="articulo_ingreso">
@@ -14,44 +28,58 @@ function Ficha() {
                         </th>
                     </tr>
                 </thead>
-
                 <tbody>
                     <tr>
                         <td>Fecha</td>
-                        <td><input type="text" value={yourDate.toISOString().split('T')[0]} /></td>
+                        <td><input id="fechaGasto" className="derecha" type="text" defaultValue={yourDate.toISOString().split('T')[0]} /></td>
                     </tr>
                     <tr>
                         <td>Hora</td>
-                        <td><input type="text" value={yourDate.toISOString().split('T')[1].substring(0,5)} /></td>
+                        <td><input id="horaGasto" className="derecha" type="text" defaultValue={yourDate.toISOString().split('T')[1].substring(0, 5)} /></td>
                     </tr>
                     <tr>
                         <td>Monto</td>
-                        <td><input type="number" /></td>
+                        <td><input id="montoGasto" className="derecha" type="number" defaultValue="0.0" /></td>
                     </tr>
                     <tr>
                         <td>Descripcion</td>
-                        <td><input type="text" /></td>
+                        <td><input id="descripcionGasto" defaultValue="" type="text" /></td>
                     </tr>
                     <tr>
                         <td>Tipo Pago</td>
-                        <td><input type="text" /></td>
+                        <td>
+                            <select id="tipoPagoGasto" name="tipo_pago">
+
+                                <option value="Efectivo" selected> Efectivo</option>
+                                <option value="Debito" > Debito</option>
+                                <option value="Transferencia" > Transferencia</option>
+                                <option value="Credito" > Credito</option>
+                                <option value="Prestado" > Prestado</option>
+                                
+
+                            </select></td>
                     </tr>
-                    <tr>
+                    {/* <tr>
                         <td>Deudor</td>
-                        <td><input type="text" /></td>
+                        <td><input id="deudorGasto" type="text" /></td>
                     </tr>
                     <tr>
                         <td>Cancelado</td>
-                        <td><input type="text" /></td>
-                    </tr>
+                        <td><input id="canceladoGasto" type="text" /></td>
+                   </tr>*/}
 
                 </tbody>
                 <tfoot>
 
                     <tr>
                         <td colSpan={2}>
-                            <button onClick={()=>{
-                                alert('Estamos en camino')
+                            <button onClick={() => {
+                                gasto.fecha = document.getElementById("fechaGasto").value;
+                                gasto.hora = document.getElementById("horaGasto").value;
+                                gasto.monto = document.getElementById("montoGasto").value;
+                                gasto.descripcion = document.getElementById("descripcionGasto").value;
+                                gasto.tipo_pago = document.getElementById("tipoPagoGasto").value;
+                                alert(JSON.stringify(gasto));
                             }}>
 
                                 Registrar
